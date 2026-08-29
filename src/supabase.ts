3a4803138bf8ae1a1904924cec790e7236b31579
Raiky Silva <125393178+RaikySilvaDEV@users.noreply.github.com>
2026-08-29 11:52:08 -0300

    feat: connect v1.3 to Supabase

---
+++ src/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://ivcbqeqsvtcblsbssvww.supabase.co',
+  'sb_publishable_qOwZYo07gB3d4Y3C23cUIQ_7qtMrrzV'
+)
